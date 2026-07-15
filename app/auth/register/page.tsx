e37009c2ff9ede18d6a3d38d50ab8419fe02c9af
Stephan Malobeka <malozdev@gmail.com>
2026-07-15 22:00:20 +0200

    first commit,initiated project and set basic project structure

---
+++ app/auth/register/page.tsx
@@ -0,0 +1,9 @@
+export default function RegisterPage() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-red-600">
+        Create Account
+      </h1>
+    </main>
+  );
+}
